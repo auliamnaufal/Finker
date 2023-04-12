25168dccbcd9bf09087b14214434029224d792d4
auliamnaufal <auliamnaufal@gmail.com>
2023-04-12 09:34:07 +0700

    Feature: add header section for home page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div class="flex justify-start items-center space-x-5">
+            <img src="{{ asset('assets/images/avatar.jpg') }}" alt="" class="w-14 h-14 object-cover object-center rounded-full shadow-lg">
+            <h1 class="text-lg font-bold">Muhammad Naufal Aulia</h1>
+        </div>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-8">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+            <div class="bg-white p-5 border border-gray-100 dark:bg-gray-800 overflow-hidden shadow-lg sm:rounded-lg space-y-2">
+               <p class="text-sm font-light">Available Balance</p>
+                <div class="w-full flex justify-start items-center space-x-2">
+                    <p class="text-xl font-bold text-gray-800">$</p>
+                    <h2 class="text-3xl font-extrabold text-black">5,000.00</h2>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <body class="font-sans antialiased max-w-lg mx-auto bg-gray-100">
         <div class="min-h-screen bg-white dark:bg-gray-900">
             @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
+                <header class="bg-white dark:bg-gray-800">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
