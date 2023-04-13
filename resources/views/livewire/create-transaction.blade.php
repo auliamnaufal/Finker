d3afdefaf4a0e650868672a44c565bb692bceb62
auliamnaufal <auliamnaufal@gmail.com>
2023-04-13 13:45:29 +0700

    Feature: create radio button for transaction type

--- resources/views/livewire/create-transaction.blade.php
+++ resources/views/livewire/create-transaction.blade.php
@@ -1,16 +1,24 @@
-<div>
-        <div class="bg-white p-4 sm:px-6 sm:py-4 border-b border-gray-150">
-            <h3 class="text-lg leading-6 font-medium text-gray-900">
-                Hello
-            </h3>
-        </div>
-        <div class="bg-white px-4 sm:p-6">
-            <div class="space-y-6">
-                mantap
-            </div>
-        </div>
+<div class="px-6 py-5">
 
-        <div class="bg-white px-4 pb-5 sm:px-4 sm:flex">
-            <button>Tesnt</button>
-        </div>
+    <h3 class="mb-5 text-lg text-center font-medium text-gray-900 dark:text-white">Create new transaction</h3>
+    <ul class="grid w-full gap-6 md:grid-cols-2">
+        <li>
+            <input type="radio" id="expense" name="type" value="expense" class="hidden peer" required checked>
+            <label for="expense" class="inline-flex items-center justify-center w-full p-3 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:bg-blue-600 peer-checked:text-white hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <div class="block">
+                    <span>Expense</span>
+                </div>
+            </label>
+        </li>
+        <li>
+            <input type="radio" id="income" name="type" value="income" class="hidden peer">
+            <label for="income" class="inline-flex items-center justify-center w-full p-3 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:bg-blue-600 peer-checked:text-white hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <div class="block">
+                    <span>Income</span>
+                </div>
+            </label>
+        </li>
+    </ul>
+
+
 </div>
