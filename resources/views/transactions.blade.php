61bc69c99fd3b0e3eaf88a6b801d0d26672d28e2
auliamnaufal <auliamnaufal@gmail.com>
2023-04-13 08:07:25 +0700

    Feature: Create transactions page

---
+++ resources/views/transactions.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex items-center space-x-4">
+            <img class="w-10 h-10 rounded-full" src="{{ asset('assets/images/avatar.jpg')  }}" alt="">
+            <div class="font-medium dark:text-white">
+                <div>Naufal Aulia</div>
+                <div class="text-sm text-gray-500 dark:text-gray-400">Last activity in 13.00 today</div>
+            </div>
+        </div>
+    </x-slot>
+
+    <div class="pb-20">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+
+
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/transactions', function () {
+        return view('transactions');
+    })->name('transactions');
 });
 
 require __DIR__.'/auth.php';
