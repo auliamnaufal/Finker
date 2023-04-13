dd5c7d1a9d1974fe7c5dc71f8d3bad83a716446e
auliamnaufal <auliamnaufal@gmail.com>
2023-04-13 09:42:09 +0700

    Styling: style base modal to fit screen width

--- resources/views/livewire/create-transaction.blade.php
+++ resources/views/livewire/create-transaction.blade.php
@@ -1,3 +1,16 @@
 <div>
+        <div class="bg-white p-4 sm:px-6 sm:py-4 border-b border-gray-150">
+            <h3 class="text-lg leading-6 font-medium text-gray-900">
+                Hello
+            </h3>
+        </div>
+        <div class="bg-white px-4 sm:p-6">
+            <div class="space-y-6">
+                mantap
+            </div>
+        </div>
 
+        <div class="bg-white px-4 pb-5 sm:px-4 sm:flex">
+            <button>Tesnt</button>
+        </div>
 </div>
--- resources/views/vendor/livewire-ui-modal/modal.blade.php
+++ resources/views/vendor/livewire-ui-modal/modal.blade.php
@@ -12,10 +12,10 @@
             x-on:close.stop="setShowPropertyTo(false)"
             x-on:keydown.escape.window="closeModalOnEscape()"
             x-show="show"
-            class="fixed inset-0 z-10 overflow-y-auto"
+            class="fixed inset-0 z-10 overflow-y-auto max-w-7xl h-screen mx-auto"
             style="display: none;"
     >
-        <div class="flex items-end justify-center min-h-screen px-4 pt-4 pb-10 text-center sm:block sm:p-0">
+        <div class="flex items-end justify-center w-full h-screen text-center">
             <div
                     x-show="show"
                     x-on:click="closeModalOnClickAway()"
@@ -30,7 +30,7 @@
                 <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
             </div>
 
-            <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+            <span class="hidden h-screen" aria-hidden="true">&#8203;</span>
 
             <div
                     x-show="show && showActiveComponent"
@@ -41,7 +41,7 @@
                     x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
                     x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     x-bind:class="modalWidth"
-                    class="inline-block w-full align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full"
+                    class="inline-block w-full h-5/6 align-bottom bg-white rounded-lg text-left sm:max-w-[32rem] overflow-hidden shadow-xl transform transition-all sm:align-middle"
                     id="modal-container"
                     x-trap.noscroll.inert="show && showActiveComponent"
                     aria-modal="true"
