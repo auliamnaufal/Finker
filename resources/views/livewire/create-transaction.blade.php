36f4e186c915d9e87b0f20684c621505d46780f6
auliamnaufal <auliamnaufal@gmail.com>
2023-05-06 20:49:13 +0700

    Fix: change transaction name input to text

--- resources/views/livewire/create-transaction.blade.php
+++ resources/views/livewire/create-transaction.blade.php
@@ -57,7 +57,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
             </div>
-            <input type="number" id="name" name="name"
+            <input type="text" id="name" name="name"
                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                    placeholder="Name">
         </div>
