8b40f694a8b6311d38e2e1b0ff96d8415b721c3e
auliamnaufal <auliamnaufal@gmail.com>
2023-05-07 13:47:47 +0700

    Refactor: change logo column in categories table to text

--- database/migrations/2023_03_06_130739_create_categories_table.php
+++ database/migrations/2023_03_06_130739_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('logo');
+            $table->text('logo');
             $table->string('color');
             $table->timestamps();
         });
