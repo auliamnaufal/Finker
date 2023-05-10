ea3d9be0b16ae0f60202ad8e286559fd61dc074f
auliamnaufal <auliamnaufal@gmail.com>
2023-05-10 13:57:11 +0700

    Feature: integrate transaction sum to pie chart

--- app/Http/Livewire/DashboardBreakdown.php
+++ app/Http/Livewire/DashboardBreakdown.php
@@ -2,17 +2,34 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Transaction;
 use Asantibanez\LivewireCharts\Models\PieChartModel;
 use Livewire\Component;
 
 class DashboardBreakdown extends Component
 {
+    public $expenseTotal;
+    public $incomeTotal;
+
+    protected $listeners = ['transactionCreated' => 'updateChart'];
+
+    public function mount() {
+        $this->expenseTotal = (int)Transaction::where('type', 'expense')->sum('amount');
+        $this->incomeTotal = (int)Transaction::where('type', 'income')->sum('amount');
+    }
+
+    public function updateChart() {
+        $this->expenseTotal = (int)Transaction::where('type', 'expense')->sum('amount');
+        $this->incomeTotal = (int)Transaction::where('type', 'income')->sum('amount');
+    }
+
     public function render()
     {
+
         $pieChartModel =
             (new PieChartModel())
-                ->addSlice('Food', 100, 'rgb(248, 180, 180)')
-                ->addSlice('Shopping', 200, 'rgb(132, 225, 188)')
+                ->addSlice('Expenses', $this->expenseTotal, 'rgb(248, 180, 180)')
+                ->addSlice('Income', $this->incomeTotal, 'rgb(132, 225, 188)')
                 ->setType('donut')
                 ->withoutLegend()
                 ->withDataLabels();
