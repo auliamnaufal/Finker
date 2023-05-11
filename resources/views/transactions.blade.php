f6875773e04bba21a059536eefda71f35295b866
auliamnaufal <auliamnaufal@gmail.com>
2023-05-11 09:09:07 +0700

    Feature: display name and last activity logged in user

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -1,11 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex items-center space-x-4">
-            <img class="w-10 h-10 rounded-full" src="{{ asset('assets/images/avatar.jpg')  }}" alt="">
-            <div class="font-medium dark:text-white">
-                <div>Naufal Aulia</div>
-                <div class="text-sm text-gray-500 dark:text-gray-400">Last activity in 13.00 today</div>
-            </div>
+        <div class="font-medium dark:text-white">
+            <div>{{ $user->name }}</div>
+            <div class="text-sm text-gray-500 dark:text-gray-400">Last Activity in {{ date_format($user->transactions->last()->created_at, 'D, d M Y : H.m a') }}</div>
         </div>
     </x-slot>
 
