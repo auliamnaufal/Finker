51a1f3e6cc661ba9582710712510aa51c49274d3
auliamnaufal <auliamnaufal@gmail.com>
2023-05-11 09:08:37 +0700

    Feature: define transaction and user relation

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -19,6 +19,10 @@ class Transaction extends Model
         'type',
     ];
 
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
     public function category() {
         return $this->belongsTo(Category::class);
     }
