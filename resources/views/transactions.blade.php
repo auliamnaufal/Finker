bd3e3c654db1d3e29a4e61d5942a5c538cd10f14
auliamnaufal <auliamnaufal@gmail.com>
2023-06-05 14:09:57 +0700

    Feature: add description if transaction is empty

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -2,7 +2,8 @@
     <x-slot name="header">
         <div class="font-medium dark:text-white">
             <div>{{ $user->name }}</div>
-            <div class="text-sm text-gray-500 dark:text-gray-400">Last Activity in {{ $user->transactions->last() ? date_format($user->transactions->last()->created_at, 'D, d M Y : H.m a') : '-' }}</div>
+            <div class="text-sm text-gray-500 dark:text-gray-400">Last Activity
+                in {{ $user->transactions->last() ? date_format($user->transactions->last()->created_at, 'D, d M Y : H.m a') : '-' }}</div>
         </div>
     </x-slot>
 
@@ -50,34 +51,34 @@
                 </div>
                 <div>
                     <div x-show="tab === 'all-tab'"
-                        x-transition
-                        x-transition:enter.duration.500ms
-                        x-transition:leave.duration.200ms
-                        class="p-4 rounded-lg bg-gray-50 dark:bg-gray-800 space-y-4 divide-y divide-gray-200"
-                        id="all">
+                         x-transition
+                         x-transition:enter.duration.500ms
+                         x-transition:leave.duration.200ms
+                         class="p-4 rounded-lg bg-gray-50 dark:bg-gray-800 space-y-4 divide-y divide-gray-200"
+                         id="all">
 
-                        <livewire:transaction-list type="all" />
+                        <livewire:transaction-list type="all"/>
 
                     </div>
                     <div x-show="tab === 'expenses-tab'"
-                        x-transition
-                        x-transition:enter.duration.500ms
-                        x-transition:leave.duration.200ms
-                        class="p-4 rounded-lg bg-gray-50 dark:bg-gray-800 space-y-4 divide-y divide-gray-200"
-                        id="expenses">
+                         x-transition
+                         x-transition:enter.duration.500ms
+                         x-transition:leave.duration.200ms
+                         class="p-4 rounded-lg bg-gray-50 dark:bg-gray-800 space-y-4 divide-y divide-gray-200"
+                         id="expenses">
 
-                        <livewire:transaction-list type="expense" />
+                        <livewire:transaction-list type="expense"/>
 
 
                     </div>
                     <div x-show="tab === 'incomes-tab'"
-                        x-transition
-                        x-transition:enter.duration.500ms
-                        x-transition:leave.duration.200ms
-                        class="p-4 rounded-lg bg-gray-50 dark:bg-gray-800 space-y-4 divide-y divide-gray-200"
-                        id="incomes">
+                         x-transition
+                         x-transition:enter.duration.500ms
+                         x-transition:leave.duration.200ms
+                         class="p-4 rounded-lg bg-gray-50 dark:bg-gray-800 space-y-4 divide-y divide-gray-200"
+                         id="incomes">
 
-                        <livewire:transaction-list type="income" />
+                        <livewire:transaction-list type="income"/>
 
                     </div>
                 </div>
@@ -86,13 +87,11 @@
         </div>
 
     @else
-    <div class="flex justify-center items-center flex-col mt-64">
-           <h2 class="text-center text-xl">If you wanna use this features upgrade to pro!</h2>
-           <br>
-            <button class="bg-sky-400 rounded-md px-5 py-2 text-white  hover:bg-sky-500 ">Upgrade Now</button>
-    </div>
-
-
+        <div class="flex justify-center items-center flex-col mt-64">
+            <h2 class="text-center text-lg font-medium mb-5">If you wanna use this features upgrade to pro!</h2>
+            <a href="{{ route('settings') }}" class="bg-sky-400 rounded-md px-5 py-2 text-white hover:bg-sky-500 ">Upgrade
+                Now</a>
+        </div>
 
     @endcan
 
